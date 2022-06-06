82f7adabe224ef2b22c5bcff1cf587e246f085eb
osoulas <oskar.soulas@gmail.com>
2022-06-06 17:38:54 +0100

    Added line for max [B] and fixed issue
    Fixed issue with the formula for [B].

--- Consecutive Reactions/Consecutive_Reactions.py
+++ Consecutive Reactions/Consecutive_Reactions.py
@@ -11,18 +11,21 @@ A0 = 1
 fig, ax = plt.subplots()
 
 t = np.linspace(0,t_end,t_end*10)
+
 A = A0*np.exp(-k1*t)
 ax.plot(t,A,label='[A]')
 
-B = ((k1*A)/(k2-k1))*(np.exp(-k1*t)-np.exp(-k2*t))
+B = ((k1*A0)/(k2-k1))*(np.exp(-k1*t)-np.exp(-k2*t))
 ax.plot(t,B,label='[B]')
 
 C = A0 - A - B
 ax.plot(t,C,label='[C]')
 
 ax.legend()
+ax.vlines(np.log(k2/k1)/(k2-k1),0,1,color='grey',linestyles='dashed',label='[B]')
 ax.set_xlabel('Time / s')
 ax.set_xbound(0,t_end)
 ax.set_ylabel('[A], [B], [C] / arbitrary units')
 ax.set_ybound(0,A0)
+
 plt.show()
