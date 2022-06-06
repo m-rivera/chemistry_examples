7008b5c6b13cd1351909de0eaada4de79a8f9c53
osoulas <oskar.soulas@gmail.com>
2022-06-06 12:37:26 +0100

    Calculates y and plots the function

--- NMR Coupling/NMR_Coupling.py
+++ NMR Coupling/NMR_Coupling.py
@@ -9,25 +9,23 @@ J_coupling_constants = [6,2]
 
 def function(x,positions,n_index):
     standard_deviation = 0.125
-    y = 0
     coefficient = 1
     for a in n[:n_index+1]:
         coefficient = (2**a)*coefficient
-    if n_index == -1:
-        coefficient = 1
+    y=x-x
     for position in positions:
-        y += (math.exp(-((x-position)**2)/(2*standard_deviation))/(standard_deviation*math.sqrt(2/math.pi)))/coefficient
+        y += (np.exp(-((x-position)**2)/(2*standard_deviation))/(standard_deviation*math.sqrt(2/math.pi)))/coefficient
     return y
-
-
-def construct():
-    positions = [0]
-    for n_index in range(len(n)):
-        for i in range(n[n_index]):
-            for i in range(len(positions)):
-                positions.append(positions[i]+J_coupling_constants[n_index]/2)
-                positions[i]=positions[i]-J_coupling_constants[n_index]/2
 
     
 fig, ax = plt.subplots()
-x = np.linspace(-20,20,100)
+x = np.linspace(-20,20,1000)
+positions = [0]
+for n_index in range(len(n)):
+    for i in range(n[n_index]):
+        for i in range(len(positions)):
+            positions.append(positions[i]+J_coupling_constants[n_index]/2)
+            positions[i]=positions[i]-J_coupling_constants[n_index]/2
+y = function(x,positions,n_index)
+ax.plot(x,y)
+plt.show()
