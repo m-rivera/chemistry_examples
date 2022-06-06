75fa5942ffed340461195cb18cfe60cb113ccd92
osoulas <oskar.soulas@gmail.com>
2022-06-06 14:07:22 +0100

    Adds more plotting functionality
    Plots each step and adds labels

--- NMR Coupling/NMR_Coupling.py
+++ NMR Coupling/NMR_Coupling.py
@@ -13,6 +13,8 @@ def function(x,positions,n_index):
     for a in n[:n_index+1]:
         coefficient = (2**a)*coefficient
     y=x-x
+    if n_index == -1:
+        coefficient = 1
     for position in positions:
         y += (np.exp(-((x-position)**2)/(2*standard_deviation))/(standard_deviation*math.sqrt(2/math.pi)))/coefficient
     return y
@@ -21,11 +23,18 @@ def function(x,positions,n_index):
 fig, ax = plt.subplots()
 x = np.linspace(-20,20,1000)
 positions = [0]
+n_index = -1
+y = function(x,positions,n_index)
+ax.plot(x,y)
+label = 'J = '
 for n_index in range(len(n)):
+    label = label+str(J_coupling_constants[n_index])+' Hz'
     for i in range(n[n_index]):
         for i in range(len(positions)):
             positions.append(positions[i]+J_coupling_constants[n_index]/2)
             positions[i]=positions[i]-J_coupling_constants[n_index]/2
-y = function(x,positions,n_index)
-ax.plot(x,y)
+    y = function(x,positions,n_index)
+    ax.plot(x,y, label=label)
+    label = label+', '
+ax.legend()
 plt.show()
