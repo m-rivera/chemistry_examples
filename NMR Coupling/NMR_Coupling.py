7366f5ea7321939a455e54c9b690b6e4fbf64145
osoulas <oskar.soulas@gmail.com>
2022-06-06 12:05:09 +0100

    Added parts of the manim script
    Kept relevant parts of the script

--- NMR Coupling/NMR_Coupling.py
+++ NMR Coupling/NMR_Coupling.py
@@ -1,1 +1,30 @@
 #NMR Coupling Visualiser
+
+import math
+
+n = [1,1]
+J_coupling_constants = [6,2]
+
+
+def function(x,positions,n_index):
+    standard_deviation = 0.125
+    y = 0
+    coefficient = 1
+    for a in n[:n_index+1]:
+        coefficient = (2**a)*coefficient
+    if n_index == -1:
+        coefficient = 1
+    for position in positions:
+        y += (math.exp(-((x-position)**2)/(2*standard_deviation))/(standard_deviation*math.sqrt(2/math.pi)))/coefficient
+    return y
+
+
+def construct():
+    positions = [0]
+    for n_index in range(len(n)):
+        for i in range(n[n_index]):
+            for i in range(len(positions)):
+                positions.append(positions[i]+J_coupling_constants[n_index]/2)
+                positions[i]=positions[i]-J_coupling_constants[n_index]/2
+
+    
