6db48dffd458efee075a4521712db46b26e11cc2
osoulas <oskar.soulas@gmail.com>
2022-06-06 16:33:54 +0100

    Add methods for plotting [A]

--- Consecutive Reactions/Consecutive_Reactions.py
+++ Consecutive Reactions/Consecutive_Reactions.py
@@ -1,1 +1,18 @@
 #Consecutive Reactions
+import matplotlib.pyplot as plt
+import numpy as np
+
+k1 = 0.1
+k2 = 1
+
+A0 = 1
+t_end = 100
+
+fig, ax = plt.subplots()
+
+t = np.linspace(0,t_end,t_end*10)
+A = A0*np.exp(-k1*t)
+ax.plot(t,A,label='[A]')
+
+ax.legend()
+plt.show()
