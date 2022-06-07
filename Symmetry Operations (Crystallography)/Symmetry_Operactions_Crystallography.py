0a53b17a57c1f0da76daccc4381aa893762e7321
osoulas <oskar.soulas@gmail.com>
2022-06-07 13:57:32 +0100

    Adds methods to calculate final coordinates

--- Symmetry Operations (Crystallography)/Symmetry_Operactions_Crystallography.py
+++ Symmetry Operations (Crystallography)/Symmetry_Operactions_Crystallography.py
@@ -1,7 +1,34 @@
 #Symmetry Operations (Crystallography)
+from re import X
 import matplotlib.pyplot as plt
 initial_coords = [1,2,3]
-sym_operation = 'x,y,-z'
+sym_operation = 'x,x+y,y-z'
 
 final_coords = [0,0,0]
 sym_operation = sym_operation.split(',')
+x = initial_coords[0]
+y = initial_coords[1]
+z = initial_coords[2]
+
+for axis, operation in enumerate(sym_operation):
+    subtract = False
+    for character in operation:
+        
+        temp = 0
+        if character == 'x':
+            temp += x
+        elif character == 'y':
+            temp += y
+        elif character == 'z':
+            temp += z
+    
+        if subtract == True:
+            temp = -temp
+
+        final_coords[axis] += temp
+        subtract = False
+
+        if character == '-':
+            subtract = True
+
+print (final_coords)
