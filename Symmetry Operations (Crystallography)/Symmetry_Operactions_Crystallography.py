bf614fcd137ca96e9a6589aa249da7e97c6520b9
osoulas <oskar.soulas@gmail.com>
2022-06-07 14:15:37 +0100

    Added plotting

--- Symmetry Operations (Crystallography)/Symmetry_Operactions_Crystallography.py
+++ Symmetry Operations (Crystallography)/Symmetry_Operactions_Crystallography.py
@@ -1,6 +1,7 @@
 #Symmetry Operations (Crystallography)
 from re import X
 import matplotlib.pyplot as plt
+
 initial_coords = [1,2,3]
 sym_operation = 'x,x+y,y-z'
 
@@ -31,4 +32,10 @@ for axis, operation in enumerate(sym_operation):
         if character == '-':
             subtract = True
 
-print (final_coords)
+
+fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+ax.plot(x, y, z, marker="o", markersize=5, color='green')
+ax.plot(final_coords[0], final_coords[1], final_coords[2], marker="o", markersize=5, color='red')
+ax.plot(0, 0, 0, marker="o", markersize=5, color='grey')
+
+plt.show()
