d64f3e23187d0ef51b81f906f91c3050dc1988ef
osoulas <oskar.soulas@gmail.com>
2022-06-14 16:32:48 +0100

    Formats documents

--- NMR_coupling/NMR_Coupling.py
+++ NMR_coupling/NMR_Coupling.py
@@ -3,29 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-n = [1,1]   # Number of H atoms per coupling
-J_coupling_constants = [6,2]    # List of J coupling constants
+n = [1, 1]   # Number of H atoms per coupling
+J_coupling_constants = [6, 2]    # List of J coupling constants
 standard_deviation = 0.125  # Standard deviation of the Gaussian curves
 
-def function(x,positions,n_index):  # Function to calculate the Gaussian curves
+
+def function(x, positions, n_index):  # Function to calculate the Gaussian curves
     coefficient = 1
     for a in n[:n_index+1]:
-        coefficient = (2**a)*coefficient    # Calculate the number of curves required
+        # Calculate the number of curves required
+        coefficient = (2**a)*coefficient
     y = np.zeros(len(x))    # Create an array of zeros to store the y values
     if n_index == -1:
         coefficient = 1
     for position in positions:
-        y += (np.exp(-((x-position)**2)/(2*standard_deviation))/(standard_deviation*math.sqrt(2/math.pi)))/coefficient # Calculates the splitting pattern by adding together Gaussian curves
+        y += (np.exp(-((x-position)**2)/(2*standard_deviation))/(standard_deviation*math.sqrt(2/math.pi))
+              )/coefficient  # Calculates the splitting pattern by adding together Gaussian curves
     return y    # Returns the y coordinates for the splitting pattern
 
-    
+
 fig, ax = plt.subplots()    # Create a figure and a 3D axes
-x = np.linspace(-20,20,1000)    # Create an array of x values
+x = np.linspace(-20, 20, 1000)    # Create an array of x values
 
 positions = [0]   # List of positions for each curve
 n_index = -1
-y = function(x,positions,n_index)   # Calculate the y coordinates for the uncoupled peak
-ax.plot(x,y, label='Uncoupled')    # Plot the uncoupled peak
+# Calculate the y coordinates for the uncoupled peak
+y = function(x, positions, n_index)
+ax.plot(x, y, label='Uncoupled')    # Plot the uncoupled peak
 
 label = 'J = '
 for n_index in range(len(n)):
@@ -33,12 +37,14 @@ for n_index in range(len(n)):
     for i in range(n[n_index]):
         for i in range(len(positions)):
             positions.append(positions[i]+J_coupling_constants[n_index]/2)
-            positions[i]=positions[i]-J_coupling_constants[n_index]/2
-    y = function(x,positions,n_index)   # Calculate the y coordinates for each additional coupling
-    ax.plot(x,y, label=label)   # Plot each coupling
+            positions[i] = positions[i]-J_coupling_constants[n_index]/2
+    # Calculate the y coordinates for each additional coupling
+    y = function(x, positions, n_index)
+    ax.plot(x, y, label=label)   # Plot each coupling
     label = label+', '
-    
-ax.legend()  # Add a legend to the figure
-ax.set_xlabel("Hz")  # Set the x label of the figure
-ax.set_yticklabels([])  # Remove the y tick labels
-plt.show()  # Display the figure
+
+ax.legend()
+ax.set_xlabel("Hz")
+ax.set_yticklabels([])
+
+plt.show()
