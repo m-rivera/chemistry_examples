3c371d905bfae07c867f32d29c7b1c80fd69a249
osoulas <oskar.soulas@gmail.com>
2022-06-16 17:17:08 +0100

    Added comments

--- lattice_symmetry_operations/lattice_symmetry_operations.py
+++ lattice_symmetry_operations/lattice_symmetry_operations.py
@@ -1,12 +1,14 @@
 # Lattice Symmetry Operations
 import matplotlib.pyplot as plt
 
+# parameters
 initial_coords = [1, 2, 3]    # initial coordinates of the atom
 x, y, z = initial_coords
 
 sym_operator = [-x, y, z]   # symmetry operatior
 x2, y2, z2 = sym_operator
 
+# plotting
 fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
 maximum = max([x, y, z, x2, y2, z2])
 minimum = min([x, y, z, x2, y2, z2])
