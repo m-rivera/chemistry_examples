4d90d92c9787e7dce3de3d7f1f18d25da94cbb41
osoulas <oskar.soulas@gmail.com>
2022-06-17 16:07:26 +0100

    Formatting

--- lever_rule/lever_rule.py
+++ lever_rule/lever_rule.py
@@ -5,27 +5,27 @@ import numpy as np
 # parameters
 pA_star = 15  # partial pressure of pure A
 pB_star = 5  # partial pressure of pure B
-units = 'Pa'    # units
+units = "Pa"  # units
 
 
 # equations
 pA = np.linspace(0, pA_star, 100)
 pB = np.linspace(pB_star, 0, 100)
-p = pA+pB
+p = pA + pB
 xA = np.linspace(0.0000000001, 1, 100)
 yA = pA / p
-zA = xA-((xA-yA) / (1+(xA/yA)))
+zA = xA - ((xA - yA) / (1 + (xA / yA)))
 
 # plotting
 fig, ax = plt.subplots()
 # plots a blue line for xA against p
-ax.plot(xA, p, color='#2369B4', label='$x_A$')
+ax.plot(xA, p, color="#2369B4", label="$x_A$")
 # plots an orange line for yA against p
-ax.plot(yA, p, color='#E66919', label='$y_A$')
+ax.plot(yA, p, color="#E66919", label="$y_A$")
 # plots a dashed line for zA against p
-ax.plot(zA, p, color='grey', label='$z_A$', linestyle='dashed')
-ax.set_xlabel('Mole Fraction of A  $z_A$')
-ax.set_ylabel('Pressure  $p$ (' + units+')')
+ax.plot(zA, p, color="grey", label="$z_A$", linestyle="dashed")
+ax.set_xlabel("Mole Fraction of A  $z_A$")
+ax.set_ylabel("Pressure  $p$ (" + units + ")")
 ax.set_xbound(0, 1)
 ax.set_ybound(0)
 ax.legend()
--- p_or_V_vs_mole_fraction/pressure_volume_vs_mole_fraction.py
+++ p_or_V_vs_mole_fraction/pressure_volume_vs_mole_fraction.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 # parameters
 pA_star = 20  # partial pressure of pure A
 pB_star = 15  # partial pressure of pure B
-units = 'Pa'    # units
+units = "Pa"  # units
 
 # plotting
 fig, ax = plt.subplots()
 
 # plots a green line between (0,B_star) and (1,0)
-ax.plot([0, 1], [pB_star, 0], color='#555F32', label='$p_B$')
+ax.plot([0, 1], [pB_star, 0], color="#555F32", label="$p_B$")
 # plots a purple line between (0,0) and (1,A_star)
-ax.plot([0, 1], [0, pA_star], color='#5F0F5F', label='$p_A$')
+ax.plot([0, 1], [0, pA_star], color="#5F0F5F", label="$p_A$")
 # plots a black line between (0,B_star) and (1,A_star)
-ax.plot([0, 1], [pB_star, pA_star], color='k', label='Total pressure $p$')
-ax.set_xlabel('Mole Fraction of A  $x_A$')
-ax.set_ylabel('Pressure  $p$ (' + units+')')
+ax.plot([0, 1], [pB_star, pA_star], color="k", label="Total pressure $p$")
+ax.set_xlabel("Mole Fraction of A  $x_A$")
+ax.set_ylabel("Pressure  $p$ (" + units + ")")
 ax.set_xbound(0, 1)
 ax.set_ybound(0)
 ax.legend()
