a2f28500c7078a3dc0eb17bb9eafc3d60622f963
osoulas <oskar.soulas@gmail.com>
2022-06-22 22:16:21 +0100

    Improved curve fill

--- particle_on_a_ring/particle_on_a_ring.py
+++ particle_on_a_ring/particle_on_a_ring.py
@@ -1,27 +1,33 @@
 """Particle On A Ring"""
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # parameters
 r = 2
-ml = -1
+ml = 5
 
 # equations
-phi = np.linspace(0,360,10000)
-#psi = (1/np.sqrt(360))*np.exp(1j*ml*phi)
-psi = np.cos(ml*phi) # real (cosine) parts of the wavefunction
+points = 3600
+phi = np.linspace(0, 360, points)
+psi = np.cos(ml * phi)  # real (cosine) parts of the wavefunction
 
 # plotting
 fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
-x = r*np.cos(phi)
-y = r*np.sin(phi)
-lines = np.linspace(0,360,360)
-for line in lines:
-    ax.plot([r*np.cos(line),r*np.cos(line)],[r*np.sin(line),r*np.sin(line)],[0,np.cos(ml*line)],alpha=0.3,color='grey')
-ax.scatter(x,y,psi,c=x-y)
-ax.plot(x,y,np.zeros(10000),alpha=0.9,color='grey')
-ax.set_xlabel('$x$')
-ax.set_ylabel('$y$')
-ax.set_zlabel('$\cos m_l\phi$')
+x = r * np.cos(phi)
+y = r * np.sin(phi)
+ax.scatter(x, y, psi, c=x - y)
+for i in range(len(x) - 1):
+    verts = [(x[i], y[i], psi[i]), (x[i + 1], y[i + 1], psi[i + 1])] + [
+        (x[i + 1], y[i + 1], np.zeros(points)[i + 1]),
+        (x[i], y[i], np.zeros(points)[i]),
+    ]
+    ax.add_collection3d(
+        Poly3DCollection([verts], color="grey", linewidths=0, alpha=0.002)
+    )
+ax.plot(x, y, np.zeros(points), color="grey")
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+ax.set_zlabel("$\cos m_l\phi$")
 
 plt.show()
