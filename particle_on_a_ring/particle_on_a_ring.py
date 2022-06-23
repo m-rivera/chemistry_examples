3d4af2b448dcbde981d142f3b9a95ff6cfd6fa80
osoulas <oskar.soulas@gmail.com>
2022-06-23 13:23:58 +0100

    Changed phi to rad and added dynamic no. of points

--- particle_on_a_ring/particle_on_a_ring.py
+++ particle_on_a_ring/particle_on_a_ring.py
@@ -5,14 +5,14 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # parameters
 r = 1
-ml = 2
+ml = 10
 
 # equations
-points = 3600
-phi = np.linspace(0, 360, points)
+points = 270*ml # number of points to be plotted
+phi = np.linspace(0, 2*np.pi, points)
 psi = np.cos(ml * phi)  # real (cosine) parts of the wavefunction
-x = r * np.cos(phi)
-y = r * np.sin(phi)
+x = r * np.cos(phi) # converts from polar to cartesian coordinates
+y = r * np.sin(phi) # converts from polar to cartesian coordinates
 
 # plotting
 fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
@@ -23,7 +23,7 @@ for i in range(len(x) - 1):
         (x[i], y[i], np.zeros(points)[i]),
     ]
     ax.add_collection3d(
-        Poly3DCollection([verts], color="grey", linewidths=0, alpha=0.002)
+        Poly3DCollection([verts], color="grey", linewidths=0, alpha=0.5)
     )
 ax.plot(x, y, np.zeros(points), color="grey")
 ax.set_xlabel("$x$")
