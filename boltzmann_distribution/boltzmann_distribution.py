d816a566449a72e16c7d757f8f9c6eec723c4beb
osoulas <oskar.soulas@gmail.com>
2022-06-23 16:47:02 +0100

    Comments

--- boltzmann_distribution/boltzmann_distribution.py
+++ boltzmann_distribution/boltzmann_distribution.py
@@ -5,13 +5,13 @@ import numpy as np
 from scipy.constants import k, eV
 
 # parameters
-T = None
-T_min = 0
-T_max = 298
+T = None # temperature (K)
+T_min = None # min temperature (K)
+T_max = None
 
-epsilon_i = None
-epsilon_0 = 0
-epsilon_i_max = 0.2
+epsilon_i = None # energy state (eV)
+epsilon_0 = None # lowest energy state (eV)
+epsilon_i_max = None # energy state boundary (eV)
 
 
 if T == epsilon_i == None:
@@ -20,7 +20,9 @@ if T == epsilon_i == None:
     T = np.linspace(T_min,T_max,10000)
     epsilon_i = np.linspace(epsilon_0,epsilon_i_max,10000)
     T, epsilon_i = np.meshgrid(T,epsilon_i)
+    # equation
     rel_occupation = np.exp(-(epsilon_i-epsilon_0)/(k/eV*T))
+    # plotting
     fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
     ax.set_xlabel('Temperature, $T$ (K)')
     ax.set_ylabel('Energy state, $\epsilon_i$ (eV)')
@@ -31,7 +33,9 @@ elif T == None:
     if T_min <= 0:
         T_min = 1e-50
     T = np.linspace(T_min,T_max,10000)
+    # equation
     rel_occupation = np.exp(-(epsilon_i-epsilon_0)/(k/eV*T))
+    # plotting
     fig, ax = plt.subplots()
     ax.set_xlabel('Temperature, $T$ (K)')
     ax.set_ylabel(r'Relative occupation, $\dfrac{n_i}{n_0}$')
@@ -40,7 +44,9 @@ elif T == None:
 
 elif epsilon_i == None:
     epsilon_i = np.linspace(epsilon_0,epsilon_i_max,10000)
+    # equation
     rel_occupation = np.exp(-(epsilon_i-epsilon_0)/(k/eV*T))
+    # plotting
     fig, ax = plt.subplots()
     ax.set_xlabel('Energy state, $\epsilon_i$ (eV)')
     ax.set_ylabel(r'Relative occupation, $\dfrac{n_i}{n_0}$')
