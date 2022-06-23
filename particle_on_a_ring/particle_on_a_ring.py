0f4dfc4531eb7c826526b35e200869d9461bf7f7
osoulas <oskar.soulas@gmail.com>
2022-06-23 13:49:18 +0100

    Adjusted comments and plot colour

--- particle_on_a_ring/particle_on_a_ring.py
+++ particle_on_a_ring/particle_on_a_ring.py
@@ -4,11 +4,11 @@ import numpy as np
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # parameters
-r = 1
-ml = 10
+r = 1 # radius of the ring
+ml = 2 # quantum number ml
 
 # equations
-points = 270*ml # number of points to be plotted
+points = 360*ml # number of points to be plotted
 phi = np.linspace(0, 2*np.pi, points)
 psi = np.cos(ml * phi)  # real (cosine) parts of the wavefunction
 x = r * np.cos(phi) # converts from polar to cartesian coordinates
@@ -16,11 +16,11 @@ y = r * np.sin(phi) # converts from polar to cartesian coordinates
 
 # plotting
 fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
-ax.scatter(x, y, psi, c=x - y)
+ax.scatter(x, y, psi, c=phi)
 for i in range(len(x) - 1):
     verts = [(x[i], y[i], psi[i]), (x[i + 1], y[i + 1], psi[i + 1])] + [
         (x[i + 1], y[i + 1], np.zeros(points)[i + 1]),
-        (x[i], y[i], np.zeros(points)[i]),
+        (x[i], y[i], np.zeros(points)[i])
     ]
     ax.add_collection3d(
         Poly3DCollection([verts], color="grey", linewidths=0, alpha=0.5)
