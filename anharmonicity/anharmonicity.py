490ceb0b9b75bd16819d5c83592aed5d2baa8858
osoulas <oskar.soulas@gmail.com>
2022-06-23 12:27:19 +0100

    Comments and formatting

--- anharmonicity/anharmonicity.py
+++ anharmonicity/anharmonicity.py
@@ -11,35 +11,35 @@ we = 298700  # ωe in m-1
 wexe = 5281.86  # ωexe in m-1
 re = 1.27455e-10  # equilibrium bond length in m
 rmax = 0.6e-9  # maximum internuclear separation in m
-vmax = 0
+vmax = 30  # number of energy levels to be plotted
 
 # equations
 atomic_mass_constant = value("atomic mass constant")
-red_mass = (m1 * m2) / (m1 + m2) * atomic_mass_constant  # equation for reduced mass
-k = red_mass * (we * 2 * pi * c) ** 2  # equation for force constant
-De = (we ** 2) / (4 * wexe)  # equation for well depth
+red_mass = (m1 * m2) / (m1 + m2) * atomic_mass_constant  # reduced mass
+k = red_mass * (we * 2 * pi * c) ** 2  # force constant
+De = (we ** 2) / (4 * wexe)  # well depth
 a = np.sqrt((2 * (pi ** 2) * red_mass * c * (we ** 2)) / (h * De))  # equation for a
 
 x = np.linspace(-re, rmax, 1000)
 r = x + re  # converts displacement to internuclear separation
 v = np.arange(vmax)
-harmonic = (0.5 * k * x ** 2) / (h * c * De)  # equation for harmonic potential
-morse = (1 - np.exp(-a * x)) ** 2  # equation for morse potential
-Gv_harmonic = we*(v+0.5)/De
-Gv_morse = (we*(v+0.5)-wexe*(v+0.5)**2)/De
+harmonic = (0.5 * k * x ** 2) / (h * c * De)  # harmonic potential
+Gv_harmonic = we * (v + 0.5) / De  # G(v) of a harmonic oscillator
+morse = (1 - np.exp(-a * x)) ** 2  # Morse potential
+Gv_morse = (we * (v + 0.5) - wexe * (v + 0.5) ** 2) / De  # G(v) of the Morse potential
 
 # plotting
 fig, ax = plt.subplots()
-ax.plot(r, harmonic,color='#1E64A5', label="Harmonic potential")
+ax.plot(r, harmonic, color="#1E64A5", label="Harmonic potential")
 for level in Gv_harmonic:
-    xmin = r[np.where(harmonic < level)][0]
-    xmax = r[np.where(harmonic < level)][-1]
-    ax.hlines(level, xmin, xmax,color='#1E64A5')
-ax.plot(r, morse,color='#FA690F', label="Morse potential")
+    rmin = r[np.where(harmonic < level)][0]
+    rmax = r[np.where(harmonic < level)][-1]
+    ax.hlines(level, rmin, rmax, color="#1E64A5")
+ax.plot(r, morse, color="#FA690F", label="Morse potential")
 for level in Gv_morse:
-    xmin = r[np.where(morse < level)][0]
-    xmax = r[np.where(morse < level)][-1]
-    ax.hlines(level, xmin, xmax,color='#FA690F')
+    rmin = r[np.where(morse < level)][0]
+    rmax = r[np.where(morse < level)][-1]
+    ax.hlines(level, rmin, rmax, color="#FA690F")
 ax.plot(
     [r[0], r[-1]],
     [1, 1],
