591f108fb374da78a19ac96b77f54b4403588b5f
osoulas <oskar.soulas@gmail.com>
2022-06-23 16:41:31 +0100

    Energy states distribution script

---
+++ boltzmann_distribution/boltzmann_distribution.py
@@ -0,0 +1,50 @@
+"""Boltzmann Distribution"""
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.constants import k, eV
+
+# parameters
+T = None
+T_min = 0
+T_max = 298
+
+epsilon_i = None
+epsilon_0 = 0
+epsilon_i_max = 0.2
+
+
+if T == epsilon_i == None:
+    if T_min <= 0:
+        T_min = 1e-50
+    T = np.linspace(T_min,T_max,10000)
+    epsilon_i = np.linspace(epsilon_0,epsilon_i_max,10000)
+    T, epsilon_i = np.meshgrid(T,epsilon_i)
+    rel_occupation = np.exp(-(epsilon_i-epsilon_0)/(k/eV*T))
+    fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+    ax.set_xlabel('Temperature, $T$ (K)')
+    ax.set_ylabel('Energy state, $\epsilon_i$ (eV)')
+    ax.set_zlabel(r'Relative occupation, $\dfrac{n_i}{n_0}$')
+    ax.plot_surface(T,epsilon_i,rel_occupation,cmap=cm.viridis,alpha=0.9)
+
+elif T == None:
+    if T_min <= 0:
+        T_min = 1e-50
+    T = np.linspace(T_min,T_max,10000)
+    rel_occupation = np.exp(-(epsilon_i-epsilon_0)/(k/eV*T))
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Temperature, $T$ (K)')
+    ax.set_ylabel(r'Relative occupation, $\dfrac{n_i}{n_0}$')
+    ax.plot(T,rel_occupation,label='$\epsilon_i='+str(epsilon_i)+'$ eV')
+    ax.legend()
+
+elif epsilon_i == None:
+    epsilon_i = np.linspace(epsilon_0,epsilon_i_max,10000)
+    rel_occupation = np.exp(-(epsilon_i-epsilon_0)/(k/eV*T))
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Energy state, $\epsilon_i$ (eV)')
+    ax.set_ylabel(r'Relative occupation, $\dfrac{n_i}{n_0}$')
+    ax.plot(epsilon_i, rel_occupation,label='$T='+str(T)+'$ K')
+    ax.legend()
+
+plt.show()
